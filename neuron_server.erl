bb1491a42c3c1a9151c5dec485365619628d7d50
Adi solomon <adi20782@gmail.com>
2020-07-31 19:15:55 +0300

    server edit

--- neuron_server.erl
+++ neuron_server.erl
@@ -32,6 +32,12 @@
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+start_supervisor(ListLayerSize, InputFile)->
+  spawn_monitor(neuron_supervisor, start/2, [ListLayerSize, InputFile]).
+
+
+%run_sys(Inputfile) edit input file and send to supervisor.
+
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
@@ -96,3 +102,5 @@ code_change(_OldVsn, State = #neuron_server_state{}, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+
