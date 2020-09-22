d8a5a9c42a782c83c4f9884cbd3da5a8fd55f3df
Adi solomon <adi20782@gmail.com>
2020-09-22 17:08:01 +0300

    TUE. python graphs functions and API

--- python_comm.erl
+++ python_comm.erl
@@ -8,13 +8,24 @@
 %%%-------------------------------------------------------------------
 -module(python_comm).
 -author("adisolo").
--compile(export_all).
-%% API
--export([]).
 
-%%---------------------------------------------------------
-%%      opening a Python instance erl-port
-%%---------------------------------------------------------
+-export([plot_graph/2]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% Spawn a python instance for graph plotting.
+%% Function is:
+%% plot_acc_vs_freq   Args=[file_data_name, start_freq]
+%% plot_val_vs_time  Args=[file_data_name]
+
+plot_graph(Function, Args)->
+  spawn(fun() -> run_python(Function, Args) end).
+
+%%%===================================================================
+%%%      opening a Python instance erl-port
+%%%===================================================================
 
 %% How to open instance
 %%%%Eshell V10.4.4  (abort with ^G)
@@ -25,9 +36,25 @@
 
 % http://erlport.org/docs/python.html#reference-manual
 
-run_python()->
+run_python(Function, Args)->
   {ok, CurrentDirectory} = file:get_cwd(),
   {ok, P}= python:start([
     {python_path, CurrentDirectory},
     {python, "python3"}]),
-  python:call(P, graph_handler, plot_waves,[]).
+  python:call(P, graph_handler, Function, string_to_binary(Args)).
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+%% transfer string to 'binary'
+%% in order to transfer to 'str' in python
+string_to_binary(List) ->
+  lists:map(
+    fun(X) -> case is_list(X) of
+                true -> list_to_binary(X);
+                _ -> X
+              end
+    end
+  , List).
